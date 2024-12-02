769e4467ba12091d342f0e1691d6cfa3dcac60a9
zapsys <zapsyssolutions@gmail.com>
2024-12-01 23:23:07 -0300

    Imported Link component

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -3,6 +3,7 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
