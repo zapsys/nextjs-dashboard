e6806148bc07b825acb9b167f42c8ea70986c6ba
zapsys <zapsyssolutions@gmail.com>
2024-12-02 18:40:50 -0300

    Segment config 'experimental_ppr' disabled

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimental_ppr = true; // Enable PPR 
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
