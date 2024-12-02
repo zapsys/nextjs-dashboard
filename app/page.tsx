579bd522a0800f64cf76a14427d8c61d95bb776f
zapsys <zapsyssolutions@gmail.com>
2024-12-01 23:08:16 -0300

    Mobile image component added

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,13 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className='block md:hidden'
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
+          <Image
             src="/hero-desktop.png"
             width={1000}
             height={760}
