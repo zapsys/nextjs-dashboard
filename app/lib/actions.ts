70211af8d3760a84ade6d17b0e9096608a47b453
zapsys <zapsyssolutions@gmail.com>
2024-12-10 20:50:12 -0300

    Revalidate path after save values into database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -24,4 +25,6 @@ export async function createInvoice(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
+      
+      revalidatePath('/dashboard/invoices');
 }
