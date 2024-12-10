1652db979cabd5d007481cdeb14bb9c1346c13d9
zapsys <zapsyssolutions@gmail.com>
2024-12-10 20:47:42 -0300

    Added sql statement to save values into database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -18,4 +19,9 @@ export async function createInvoice(formData: FormData) {
       });
       const amountInCents = amount * 100;
       const date = new Date().toISOString().split('T')[0];
+
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
 }
