deae21a1733562b05b270498824708da0c2ad01b
zapsys <zapsyssolutions@gmail.com>
2024-12-10 20:51:53 -0300

    Redirect to '/dashboard/invoices' page after data submission

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -25,6 +26,8 @@ export async function createInvoice(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
-      
+
       revalidatePath('/dashboard/invoices');
+      redirect('/dashboard/invoices');
+      
 }
