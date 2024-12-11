c2c7b2bbf9f7ea6fa51b5e1dd45f1279f1ddad9a
zapsys <zapsyssolutions@gmail.com>
2024-12-10 21:12:34 -0300

    New action 'updateInvoice' created

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,9 @@ const FormSchema = z.object({
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
@@ -31,5 +34,23 @@ export async function createInvoice(formData: FormData) {
 
       revalidatePath('/dashboard/invoices');
       redirect('/dashboard/invoices');
-      
+
+}
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
